cddf4f1d212a421d649444e837e60e7b7356f5f7
권구현 <61926067+milkid1412@users.noreply.github.com>
2022-08-05 14:40:58 +0900

    Update TimeLimitItem.php

--- src/Kkevin14/TimeLimitItem/TimeLimitItem.php
+++ src/Kkevin14/TimeLimitItem/TimeLimitItem.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 namespace Kkevin14\TimeLimitItem;
 
 use Kkevin14\TimeLimitItem\command\MainCommand;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 
 class TimeLimitItem extends PluginBase
 {
-    public function onEnable()
+    protected function onEnable()
     {
         $this->getServer()->getCommandMap()->register('Kkevin14', new MainCommand($this));
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
@@ -17,6 +17,6 @@ class TimeLimitItem extends PluginBase
 
     public function msg(Player $player, string $msg): void
     {
-        $player->sendMessage('§e< §f서버 §e> §f' . $msg);
+        $player->sendMessage('§e< §f서버 §e> §7' . $msg);
     }
 }
